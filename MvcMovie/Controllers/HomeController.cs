91fe44d2352efecc9c3bf9fc0938eb0416ed6036
Tim Glenn <timglenn@nimbusblue.com>
2018-02-01 19:17:17 -0700

    bubblesort.

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -15,6 +15,30 @@ namespace MvcMovie.Controllers
             return "Lubba Dubba Lub Dub!!! Oh! This is weirding me out, Let's get out of here Morty!";
         }
 
+        public string BubbleSort(string data)
+        {
+            //Compare first two, take the largest as result
+            //take result compare to next element
+            //var elements = data.Split(',');
+            int[] elements = { 64, 52, 11, 23, 45, 6, 77, 3, 4, 2 };
+
+            for (int i = 0; i < elements.Length - 1; i++)
+            {
+                for (int j = 0; j < elements.Length - i - 1; j++)
+                {
+                    if (elements[j] > elements[j + 1])
+                    {
+                        var temp = elements[j];
+                        elements[j] = elements[j + 1];  //since the next item is less than elements[i] lets set it as i+1
+                        elements[j + 1] = temp; //move the original elements[i] into the new spot
+
+                        System.Diagnostics.Debug.WriteLine("element has been swapped: {0}", elements[j]);
+                    }
+                }
+            }
+            return String.Join(",", elements);
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
